c4221877b3de5eac2c5c4cc75634e724d5f3a58b
BPKiller <BPKiller@BPKiller-PC.lan>
2015-12-15 12:16:09 +0700

    Modified Exception

--- src/exception/GameOverException.java
+++ src/exception/GameOverException.java
@@ -1,8 +1,11 @@
 package exception;
 
+import entity.Player;
+
 public class GameOverException extends Exception {
 	public GameOverException() {
-		
+		System.out.println("The game is over.");
+		System.out.println("Your score is " +Player.getScore());
 	}
 
 }
